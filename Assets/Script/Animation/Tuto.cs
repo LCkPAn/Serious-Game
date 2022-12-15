b0072384bff7ae2f9a58d7806d31bf54b90fe695
LCkPAn <92451567+LCkPAn@users.noreply.github.com>
2022-12-15 12:31:59 +0100

    TUTO En cours

---
+++ Assets/Script/Animation/Tuto.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening; 
+
+public class Tuto : MonoBehaviour
+{
+    /*public Image tutoImage; 
+
+    void ActiveTuto()
+    {
+     transform.DOMoveX(0.8f, 2).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Restart);
+     tutoImage.DOFade(0.1f, 1f).SetLoops(1, LoopType.Restart);
+    }
+    */
+}
