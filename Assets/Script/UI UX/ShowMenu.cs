429f9049d78abfe7c1f5f1c3d9378351f2467d3b
LCkPAn <92451567+LCkPAn@users.noreply.github.com>
2023-01-06 17:04:23 +0100

    tuto menu ok

--- Assets/Script/UI UX/ShowMenu.cs
+++ Assets/Script/UI UX/ShowMenu.cs
@@ -9,18 +9,21 @@ public class ShowMenu : MonoBehaviour
     public Image buttonPlay;
     public Image logoPlay;
     public Button ButtonMenu;
+    public GameObject entryDoor;
 
     private void Start()
     {
         ButtonMenu.interactable = false;
         buttonPlay.DOFade(0, 0);
         logoPlay.DOFade(0, 0);
+        entryDoor.GetComponent<MeshRenderer>().material.DOFade(0.5f, 1).SetLoops(-1, LoopType.Yoyo);
     }
 
     private void OnMouseDown()
     {
         buttonPlay.DOFade(1, 2);
         logoPlay.DOFade(1, 2);
+        Destroy(entryDoor);
         StartCoroutine(MyCourtine());
     }
 
